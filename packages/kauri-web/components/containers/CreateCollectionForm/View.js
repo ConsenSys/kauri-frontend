ef2431ffdbba44e72ea79b12912650b16bc5ef9c
Eric Juta <ericjohn.juta@consensys.net>
2018-09-25 12:46:54 +0100

    Import shizzle

--- packages/kauri-web/components/containers/CreateCollectionForm/View.js
+++ packages/kauri-web/components/containers/CreateCollectionForm/View.js
@@ -2,6 +2,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Form, Field, FieldArray } from 'formik'
+import Stack from 'stack-styled'
+import { space } from 'styled-system'
+import ActionsSection from '../../../../kauri-components/components/Section/ActionsSection'
+import PrimaryHeaderSection from '../../../../kauri-components/components/Section/PrimaryHeaderSection'
+import AddTagButton from '../../../../kauri-components/components/Button/AddTagButton'
+import PrimaryButton from '../../../../kauri-components/components/Button/PrimaryButton'
+import TertiaryButton from '../../../../kauri-components/components/Button/TertiaryButton'
+import AddMemberButton from '../../../../kauri-components/components/Button/AddMemberButton'
+import ProfileHeaderLabel from '../../../../kauri-components/components/PublicProfile/ProfileHeaderLabel.bs'
+import StatisticsContainer from '../../../../kauri-components/components/PublicProfile/StatisticsContainer.bs'
+import UserWidgetSmall from '../../../../kauri-components/components/UserWidget/UserWidgetSmall.bs'
+import CuratorHeaderLabel from '../../../../kauri-components/components/Typography/CuratorHeaderLabel'
+import Input from '../../../../kauri-components/components/Input/Input'
+
 import type { FormState } from './index'
 
 type Props = {
@@ -17,15 +31,105 @@ type Props = {
 const Section = styled.section`
   display: flex;
   flex-direction: column;
-  padding: ${props => props.theme.padding};
+  padding: 0px ${props => props.theme.padding};
 `
 
-export default ({ touched, errors }: Props) => <Section>
-  <Form>
-    <div className='control'>
-      <label className='label'>Email Address</label>
-      {touched.email && typeof errors.email === 'string' && <p>{errors.email}</p>}
-      <Field className='input' type='email' name='email' placeholder='Email' />
-    </div>
-  </Form>
-</Section>
+const CreateCollectionDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  > :not(:last-child) {
+    ${space};
+  }
+`
+
+const CreateCollectionActionsPlaceHolder = styled.div`
+  display: flex;
+  mix-blend-mode: normal;
+  opacity: 0.2;
+  cursor: initial !important;
+  > * {
+    ${space};
+    cursor: initial !important;
+  }
+`
+
+const CreateCollectionMetaDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > * {
+    ${space};
+  }
+`
+
+const CreateCollectionCuratorDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > :first-child {
+    ${space};
+  }
+`
+
+const CreateCollectionCurators = styled.div`
+  display: flex;
+  align-items: center;
+  > * {
+    ${space};
+  }
+`
+
+const UploadIcon = () => <img src='https://png.icons8.com/color/50/000000/upload.png' />
+
+export default ({ touched, errors }: Props) =>
+  <Section>
+    <Form>
+      <ActionsSection>
+        <Stack alignItems={['', 'center']} >
+          <TertiaryButton icon={<img src='https://png.icons8.com/flat_round/50/000000/back.png' />}>Cancel Collection</TertiaryButton>
+        </Stack>
+        <Stack alignItems={['', 'center']} justifyContent={['', 'center']}>
+          <TertiaryButton icon={<UploadIcon />}handleClick={() => alert('clicked')}>Background Image</TertiaryButton>
+        </Stack>
+        <Stack alignItems={['', 'center']} justifyContent={['', 'end']}>
+          <PrimaryButton>Create</PrimaryButton>
+        </Stack>
+      </ActionsSection>
+
+      <PrimaryHeaderSection>
+        <CreateCollectionDetails mb={2}>
+          <ProfileHeaderLabel header='Collection' />
+          <Input placeHolder='Add collection title' fontSize={5} />
+          <Input placeHolder='Add description' fontSize={3} />
+          <AddTagButton color='white' />
+          <CreateCollectionActionsPlaceHolder mr={3}>
+            <PrimaryButton>Follow Collection</PrimaryButton>
+            <TertiaryButton>Up vote</TertiaryButton>
+            <TertiaryButton>Share</TertiaryButton>
+          </CreateCollectionActionsPlaceHolder>
+        </CreateCollectionDetails>
+        <Stack alignItems={['', 'center']} justifyContent={['', 'end']}>
+          <CreateCollectionMetaDetails mb={4}>
+            <StatisticsContainer
+              pageType='CollectionPage'
+              statistics={
+                [
+                  { 'name': 'Followers', 'count': 0 },
+                  { 'name': 'Articles', 'count': 0 },
+                  { 'name': 'Views', 'count': 0 },
+                  { 'name': 'Upvotes', 'count': 0 },
+                ]
+              }
+            />
+            <CreateCollectionCuratorDetails mb={2}>
+              <CuratorHeaderLabel>Curator</CuratorHeaderLabel>
+              <CreateCollectionCurators mr={3}>
+                <UserWidgetSmall color='FFFFFF' username={'davodesign84'} />
+                <AddMemberButton />
+              </CreateCollectionCurators>
+            </CreateCollectionCuratorDetails>
+          </CreateCollectionMetaDetails>
+        </Stack>
+      </PrimaryHeaderSection>
+    </Form>
+  </Section>
--- packages/kauri-web/components/containers/CreateCollectionForm/index.js
+++ packages/kauri-web/components/containers/CreateCollectionForm/index.js
@@ -14,28 +14,28 @@ export type FormState = {
 }
 
 export default compose(
-  withFormik({
-    mapPropsToValues ({ email, password, newsletter, editor, test }) {
-      return {
-        email: email || '',
-        password: password || '',
-        newsletter: newsletter || false,
-        editor: editor || 'atom',
-        test: test || '',
-      }
-    },
-    validationSchema: Yup.object().shape({
-      email: Yup.string().email('Email not valid').required('Email is required'),
-    }),
-    handleSubmit (values, { resetForm, setErrors, setSubmitting }) {
-      setTimeout(() => {
-        if (values.email === 'yomi@gmail.io') {
-          setErrors({ email: 'That email is already taken' })
-        } else {
-          resetForm()
-        }
-        setSubmitting(false)
-      }, 2000)
-    },
-  })
+  // withFormik({
+  //   mapPropsToValues ({ email, password, newsletter, editor, test }) {
+  //     return {
+  //       email: email || '',
+  //       password: password || '',
+  //       newsletter: newsletter || false,
+  //       editor: editor || 'atom',
+  //       test: test || '',
+  //     }
+  //   },
+  //   validationSchema: Yup.object().shape({
+  //     email: Yup.string().email('Email not valid').required('Email is required'),
+  //   }),
+  //   handleSubmit (values, { resetForm, setErrors, setSubmitting }) {
+  //     setTimeout(() => {
+  //       if (values.email === 'yomi@gmail.io') {
+  //         setErrors({ email: 'That email is already taken' })
+  //       } else {
+  //         resetForm()
+  //       }
+  //       setSubmitting(false)
+  //     }, 2000)
+  //   },
+  // })
 )(CreateCollectionForm)
