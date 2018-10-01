2acf2c65cd9b415003d4a55fcb0ed59e38450a9d
Eric Juta <ericjohn.juta@consensys.net>
2018-10-01 13:25:05 +0100

    Fix includes because webpack config sigh

--- packages/kauri-components/.storybook/webpack.config.js
+++ packages/kauri-components/.storybook/webpack.config.js
@@ -8,12 +8,13 @@ module.exports = (storybookBaseConfig, configType) => {
   // 'PRODUCTION' is used when building the static version of storybook.
 
   // Make whatever fine-grained changes you need
-  // console.log(storybookBaseConfig)
   const webpack = require('webpack')
+  storybookBaseConfig.module.rules[0].include.push(path.join(__dirname, '../../kauri-web'))
   storybookBaseConfig.module.rules[0].use[0].loader = require.resolve('babel-loader')
   storybookBaseConfig.module.rules[0].use[0].options.presets = babelConfig.presets
   storybookBaseConfig.module.rules[0].use[0].options.plugins = babelConfig.plugins
   storybookBaseConfig.plugins.push(new webpack.EnvironmentPlugin(['STORYBOOK']))
+  // console.log(storybookBaseConfig)
 
   // Return the altered config
   return storybookBaseConfig
