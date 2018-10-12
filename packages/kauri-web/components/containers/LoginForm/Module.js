4d781b329210632c120e763f7468d4c931049d6e
Eric Juta <ericjohn.juta@consensys.net>
2018-10-12 16:31:11 +0100

    Update Module.js

--- packages/kauri-web/components/containers/LoginForm/Module.js
+++ packages/kauri-web/components/containers/LoginForm/Module.js
@@ -78,16 +78,17 @@ export const registerEpic = (action$: Observable<RegisterAction>, store: any, { 
           .do(h => console.log(h))
           .do(() => callback())
           .do(({ token }: FinalLoginResponse) => {
-            if (process.env.NODE_ENV === 'development') {
+            
               console.log(token)
               console.log(window.web3.eth.accounts[0])
               document.cookie = cookie.serialize('TOKEN', token, {
-                maxAge: 30 * 24 * 60 * 60, // 30 days
+                maxAge: 30 * 24 * 60 * 60, // 30 days,
+                domain: "." + window.location.hostname
               })
               document.cookie = cookie.serialize('USER_ID', window.web3.eth.accounts[0], {
                 maxAge: 30 * 24 * 60 * 60, // 30 days
               })
-            }
+          
           })
           .mergeMapTo(
             Observable.of(
