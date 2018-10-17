10e1418ceb7be3eadfe91925a90597de1e33a8af
Eric Juta <ericjohn.juta@consensys.net>
2018-10-17 17:45:38 +0100

    Davide and me need a pay rise

--- packages/kauri-web/components/containers/Link/index.js
+++ packages/kauri-web/components/containers/Link/index.js
@@ -1,7 +1,12 @@
-//@flow
+// @flow
 import { connect } from 'react-redux'
 import { compose } from 'react-apollo'
 import { trackAnalyticsAction } from './Module'
 import Link from './View'
 
-export default compose(connect(() => ({}), { trackAnalyticsAction }))(Link)
+export default compose(
+  connect(
+    () => ({}),
+    { trackAnalyticsAction, routeChangeAction: payload => ({ type: 'ROUTE_CHANGE', payload }) }
+  )
+)(Link)
--- packages/kauri-web/components/containers/LoginForm/Module.js
+++ packages/kauri-web/components/containers/LoginForm/Module.js
@@ -7,10 +7,8 @@ import { showNotificationAction } from '../../../lib/Module'
 import { trackMixpanelAction } from '../Link/Module'
 import { loginPersonalSign } from '../../../lib/web3-personal-sign'
 import superagent from 'superagent'
-import nextRoutes from 'next-routes'
 import type { Dependencies } from '../../../lib/Module'
 const config = require('../../../config').default
-const Router = nextRoutes().Router
 
 const request = superagent.agent()
 
@@ -39,16 +37,16 @@ type InitiateLoginResponse = {
   date_created: number,
   date_expiration: number,
   app_id: string,
-  active: boolean
-};
+  active: boolean,
+}
 
 type FinalLoginResponse = {
   app_id: string,
   client_id: string,
   address: string,
   expiration: number,
-  token: string
-};
+  token: string,
+}
 
 const registerSignaturePayload = (userId, signature, sentence_id) => ({
   address: userId,
@@ -62,7 +60,7 @@ export const registerEpic = (action$: Observable<RegisterAction>, store: any, { 
   action$.ofType(REGISTER).switchMap(({ payload: { type = 'register' }, callback }: RegisterAction) =>
     Observable.fromPromise(
       request
-      // http://api.dev2.kauri.io/web3auth/api/login?app_id=kauri&client_id=kauri-gateway
+        // http://api.dev2.kauri.io/web3auth/api/login?app_id=kauri&client_id=kauri-gateway
         .get(`https://${config.getApiURL()}/web3auth/api/login?app_id=${config.appId}&client_id=${config.clientId}`)
     )
       .map(res => res.body)
@@ -70,11 +68,7 @@ export const registerEpic = (action$: Observable<RegisterAction>, store: any, { 
       .switchMap(({ sentence, id }: InitiateLoginResponse) =>
         loginPersonalSign(sentence)
           .map((signature: string) => registerSignaturePayload(window.web3.eth.accounts[0], signature, id))
-          .mergeMap((payload) =>
-            request
-              .post(`https://${config.getApiURL()}/web3auth/api/login`)
-              .send(payload)
-          )
+          .mergeMap(payload => request.post(`https://${config.getApiURL()}/web3auth/api/login`).send(payload))
           .map(res => res.body)
           .do(h => console.log(h))
           .do(() => callback())
@@ -83,11 +77,15 @@ export const registerEpic = (action$: Observable<RegisterAction>, store: any, { 
             console.log(window.web3.eth.accounts[0])
             document.cookie = cookie.serialize('TOKEN', token, {
               maxAge: 30 * 24 * 60 * 60, // 30 days,
-              domain: window.location.hostname.includes('localhost') ? window.location.hostname : '.' + window.location.hostname,
+              domain: window.location.hostname.includes('localhost')
+                ? window.location.hostname
+                : '.' + window.location.hostname,
             })
             document.cookie = cookie.serialize('USER_ID', window.web3.eth.accounts[0], {
               maxAge: 30 * 24 * 60 * 60, // 30 days
-              domain: window.location.hostname.includes('localhost') ? window.location.hostname : '.' + window.location.hostname,
+              domain: window.location.hostname.includes('localhost')
+                ? window.location.hostname
+                : '.' + window.location.hostname,
             })
           })
           .mergeMapTo(
@@ -110,7 +108,7 @@ export const registerEpic = (action$: Observable<RegisterAction>, store: any, { 
           )
           .delay(500)
           .do(() => {
-            window.location =  `/edit-profile` + window.location.search;
+            window.location = `/edit-profile` + window.location.search
           })
           .catch(err => {
             console.error(err)
