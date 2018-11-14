6a1a77f118a8cccabec278d015d351da5a9f45a1
Davide Scalzo <scalzodavide@yahoo.it>
2018-11-14 14:02:48 +0100

    fix curated list query article count

--- packages/kauri-admin/src/queries/curatedList.js
+++ packages/kauri-admin/src/queries/curatedList.js
@@ -74,6 +74,10 @@ export const getAllCuratedList = (payload, maxResult, filter) => ({
                     id
                   }
                 }
+                resourcesId {
+                  id
+                  type
+                }
               }
             }
           }
