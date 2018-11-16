467a70f16a622a5fa271f9cdce827e4c5a270035
Eric Juta <ericjohn.juta@consensys.net>
2018-11-16 20:39:48 +0000

    Update webpack.config.prod.js

--- packages/kauri-admin/config/webpack.config.prod.js
+++ packages/kauri-admin/config/webpack.config.prod.js
@@ -173,7 +173,7 @@ module.exports = {
     // https://github.com/facebook/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.ts', '.tsx'],
     alias: {
       
       // Support React Native Web
