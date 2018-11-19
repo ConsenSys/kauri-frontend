c2fb779aa1cf07338aa2c59d010cfc470c83742b
Eric Juta <ericjohn.juta@consensys.net>
2018-11-19 14:09:53 +0000

    Colour of update article button

--- packages/kauri-web/components/containers/Article/ApprovedArticle/ApprovedArticleContent.js
+++ packages/kauri-web/components/containers/Article/ApprovedArticle/ApprovedArticleContent.js
@@ -1,20 +1,23 @@
 // @flow
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { EditorState, convertFromRaw } from 'draft-js'
-import { Link } from '../../../../routes'
-import slugify from 'slugify'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { EditorState, convertFromRaw } from 'draft-js';
+import { Link } from '../../../../routes';
+import slugify from 'slugify';
 import {
   CreateRequestContent as SubmitArticleFormContent,
   CreateRequestContainer as SubmitArticleFormContainer,
   CreateRequestDetails,
-} from '../../CreateRequestForm/CreateRequestContent'
-import DescriptionRow from '../../Requests/DescriptionRow'
-import { contentStateFromHTML, getHTMLFromMarkdown } from '../../../../lib/markdown-converter-helper'
-import ShareArticle from '../../../../../kauri-components/components/Tooltip/ShareArticle'
-import Outline from '../../../../../kauri-components/components/Typography/Outline.bs'
-import TertiaryButton from '../../../../../kauri-components/components/Button/TertiaryButton'
-import userIdTrim from '../../../../lib/userid-trim'
+} from '../../CreateRequestForm/CreateRequestContent';
+import DescriptionRow from '../../Requests/DescriptionRow';
+import {
+  contentStateFromHTML,
+  getHTMLFromMarkdown,
+} from '../../../../lib/markdown-converter-helper';
+import ShareArticle from '../../../../../kauri-components/components/Tooltip/ShareArticle';
+import Outline from '../../../../../kauri-components/components/Typography/Outline.bs';
+import TertiaryButton from '../../../../../kauri-components/components/Button/TertiaryButton';
+import userIdTrim from '../../../../lib/userid-trim';
 
 export const ApprovedArticleDetails = CreateRequestDetails.extend`
   align-items: inherit;
@@ -28,38 +31,48 @@ export const ApprovedArticleDetails = CreateRequestDetails.extend`
   @media (max-width: 500px) {
     display: none;
   }
-`
+`;
 
 const UpdateArticleSvgIcon = () => (
-  <svg aria-hidden='true' data-prefix='fas' data-icon='file' role='img' viewBox='0 0 384 512'>
+  <svg
+    aria-hidden='true'
+    data-prefix='fas'
+    data-icon='file'
+    role='img'
+    viewBox='0 0 384 512'
+  >
     <path
       fill='#0BA986'
       d='M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm160-14.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z'
     />
   </svg>
-)
+);
 
 const ApprovedArticleHelmet = ({ blocks, contentState }) => {
-  if (!blocks) return
+  if (!blocks) return;
 
-  let description = blocks.filter(({ text }) => text.length >= 40)
+  let description = blocks.filter(({ text }) => text.length >= 40);
   description = description.length
     ? description.find(block => block.type.includes('unstyled')) &&
       description.find(block => block.type.includes('unstyled')).text
     : blocks.find(block => block.type.includes('unstyled')) &&
-      blocks.find(block => block.type.includes('unstyled')).text
-  description = description && description.length > 120 ? description.substring(0, 117) + '...' : description
-  let imageEntityKey = contentState && contentState.getLastCreatedEntityKey()
-  let image = parseInt(imageEntityKey) && contentState.getEntity(imageEntityKey).toJS()
-  if (image) image = image.data.src
+      blocks.find(block => block.type.includes('unstyled')).text;
+  description =
+    description && description.length > 120
+      ? description.substring(0, 117) + '...'
+      : description;
+  let imageEntityKey = contentState && contentState.getLastCreatedEntityKey();
+  let image =
+    parseInt(imageEntityKey) && contentState.getEntity(imageEntityKey).toJS();
+  if (image) image = image.data.src;
 
   return (
     <Helmet>
       <meta name='description' content={description} />
       {image && <meta name='image' content={image} />}
     </Helmet>
-  )
-}
+  );
+};
 
 export default ({
   text,
@@ -89,7 +102,8 @@ export default ({
   hostName: string,
   resourceType: 'user' | 'community',
 }) => {
-  let editorState = typeof text === 'string' && text[0] === '{' && JSON.parse(text)
+  let editorState =
+    typeof text === 'string' && text[0] === '{' && JSON.parse(text);
   if (!editorState) {
     return (
       <SubmitArticleFormContent>
@@ -97,15 +111,15 @@ export default ({
           <span>{text}</span>
         </p>
       </SubmitArticleFormContent>
-    )
+    );
   }
   editorState =
     editorState && typeof editorState.markdown === 'string'
       ? editorState
-      : EditorState.createWithContent(convertFromRaw(JSON.parse(text)))
+      : EditorState.createWithContent(convertFromRaw(JSON.parse(text)));
   const contentState = editorState.markdown
     ? contentStateFromHTML(getHTMLFromMarkdown(editorState.markdown))
-    : editorState.getCurrentContent()
+    : editorState.getCurrentContent();
 
   const blocks =
     typeof editorState === 'object' &&
@@ -114,9 +128,11 @@ export default ({
       : editorState
         .getCurrentContent()
         .getBlocksAsArray()
-        .map(block => block.toJS()))
+        .map(block => block.toJS()));
 
-  const outlineHeadings = blocks.filter(({ type }) => type.includes('header-one')).map(({ text }) => text)
+  const outlineHeadings = blocks
+    .filter(({ type }) => type.includes('header-one'))
+    .map(({ text }) => text);
 
   return (
     <SubmitArticleFormContent>
@@ -129,35 +145,50 @@ export default ({
           linkComponent={children => (
             <Link
               useAnchorTag
-              href={resourceType === 'community' ? `/community/${ownerId}` : `/public-profile/${ownerId || authorId}`}
+              href={
+                resourceType === 'community'
+                  ? `/community/${ownerId}`
+                  : `/public-profile/${ownerId || authorId}`
+              }
             >
               {children}
             </Link>
           )}
           headings={outlineHeadings || []}
           username={username}
-          userId={(ownerId && userIdTrim(ownerId)) || (authorId && userIdTrim(authorId))}
+          userId={
+            (ownerId && userIdTrim(ownerId)) ||
+            (authorId && userIdTrim(authorId))
+          }
           userAvatar={userAvatar}
           text={ownerId ? 'OWNER' : 'AUTHOR'}
           routeChangeAction={routeChangeAction}
         />
         <TertiaryButton
+          color={'textPrimary'}
           icon={<UpdateArticleSvgIcon />}
           handleClick={() =>
             userId
-              ? routeChangeAction(`/article/${article_id}/v${article_version}/update-article`)
-              : routeChangeAction(`/login?r=article/${article_id}/v${article_version}/update-article`)
+              ? routeChangeAction(
+                `/article/${article_id}/v${article_version}/update-article`
+              )
+              : routeChangeAction(
+                `/login?r=article/${article_id}/v${article_version}/update-article`
+              )
           }
         >
           Update article
         </TertiaryButton>
         <ShareArticle
-          url={`${hostName.replace(/api\./g, '')}/article/${article_id}/${slugify(subject, {
+          url={`${hostName.replace(
+            /api\./g,
+            ''
+          )}/article/${article_id}/${slugify(subject, {
             lower: true,
           })}`}
           title={subject}
         />
       </ApprovedArticleDetails>
     </SubmitArticleFormContent>
-  )
-}
+  );
+};
