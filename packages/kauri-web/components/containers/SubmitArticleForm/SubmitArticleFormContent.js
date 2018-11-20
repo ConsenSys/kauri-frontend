5710852b51117a8b7e61ff94803b8c4cadd83051
Eric Juta <ericjohn.juta@consensys.net>
2018-11-20 10:46:52 +0000

    Cleanup Outline

--- packages/kauri-web/components/containers/SubmitArticleForm/SubmitArticleFormContent.js
+++ packages/kauri-web/components/containers/SubmitArticleForm/SubmitArticleFormContent.js
@@ -1,18 +1,13 @@
 // @flow
-import React from 'react'
-import { EditorState, ContentState } from 'draft-js'
-import SharedEditor from '../../common/SharedEditor'
-import styled from 'styled-components'
+import React from 'react';
+import { EditorState, ContentState } from 'draft-js';
+import SharedEditor from '../../common/SharedEditor';
+import styled from 'styled-components';
 import {
   CreateRequestContent as SubmitArticleFormContent,
   CreateRequestContainer as SubmitArticleFormContainer,
-} from '../CreateRequestForm/CreateRequestContent'
-import { contentStateFromHTML, getHTMLFromMarkdown } from '../../../lib/markdown-converter-helper'
-import Outline from '../../../../kauri-components/components/Typography/Outline.bs'
-import userIdTrim from '../../../lib/userid-trim'
-import { ApprovedArticleDetails as SubmitArticleFormDetails } from '../Article/ApprovedArticle/ApprovedArticleContent'
-
-import type { EditArticlePayload, SubmitArticlePayload } from './Module'
+} from '../CreateRequestForm/CreateRequestContent';
+import type { EditArticlePayload, SubmitArticlePayload } from './Module';
 
 type Props =
   | any
@@ -30,37 +25,44 @@ type Props =
       setFieldsValue: ({ text: string }) => void,
       getFieldError: string => any,
       text?: string,
-    }
+    };
 
 type State = {
   editorState: any,
-}
+};
 
 class SubmitArticleFormText extends React.Component<Props, State> {
   constructor (props) {
-    super(props)
+    super(props);
     if (props.text) {
-      const rawData = ContentState.createFromText(JSON.parse(props.text).markdown)
-      const newEditorState = EditorState.createWithContent(rawData)
+      const rawData = ContentState.createFromText(
+        JSON.parse(props.text).markdown
+      );
+      const newEditorState = EditorState.createWithContent(rawData);
 
       this.state = {
         editorState: { draftEditorState: newEditorState },
-      }
+      };
     } else {
       this.state = {
-        editorState: { markdown: 'Write markdown content here!', text: 'Write markdown content here!' },
-      }
+        editorState: {
+          markdown: 'Write markdown content here!',
+          text: 'Write markdown content here!',
+        },
+      };
     }
   }
 
   componentDidMount () {
     if (this.props.text) {
-      const rawData = ContentState.createFromText(JSON.parse(this.props.text).markdown)
-      const newEditorState = EditorState.createWithContent(rawData)
+      const rawData = ContentState.createFromText(
+        JSON.parse(this.props.text).markdown
+      );
+      const newEditorState = EditorState.createWithContent(rawData);
 
       this.state = {
         editorState: { draftEditorState: newEditorState },
-      }
+      };
     }
   }
 
@@ -69,9 +71,10 @@ class SubmitArticleFormText extends React.Component<Props, State> {
       {
         editorState,
       },
-      () => this.props.setFieldsValue({ text: editorState && editorState.markdown })
-    )
-  }
+      () =>
+        this.props.setFieldsValue({ text: editorState && editorState.markdown })
+    );
+  };
 
   render () {
     return this.props.getFieldDecorator('text', {
@@ -82,68 +85,19 @@ class SubmitArticleFormText extends React.Component<Props, State> {
           whitespace: true,
         },
       ],
-      initialValue: typeof this.props.text === 'string' ? JSON.stringify({ markdown: this.props.text }) : null,
+      initialValue:
+        typeof this.props.text === 'string'
+          ? JSON.stringify({ markdown: this.props.text })
+          : null,
     })(
       <SharedEditor
         hasErrors={this.props.getFieldError('text')}
         editorState={this.state.editorState}
         handleChange={this.handleChange}
       />
-    )
+    );
   }
 }
-
-const OutlineHeader = styled.h5`
-  margin-bottom: 20px;
-  color: ${props => props.theme.primaryColor};
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 18px;
-  cursor: pointer;
-  :hover {
-    color: ${props => props.theme.secondaryTextColor};
-  }
-`
-
-export const SubmitArticleFormHeadings = ({ editorState }: *) => {
-  // typeof editorState === 'object' && console.log(contentStateFromHTML(getHTMLFromMarkdown(editorState.markdown)))
-
-  return (
-    <div>
-      {/* {console.log(contentStateFromHTML(getHTMLFromMarkdown(editorState.markdown)))} */}
-      {outlineHeadings &&
-        outlineHeadings.map((header, i) => (
-          <OutlineHeader
-            onClick={() => {
-              const outlineHeaderHash = header.text
-                .replace(/ /g, '')
-                .replace(/\W|_/g, '')
-                .toLowerCase()
-              const headerDOMNode = document.getElementById(outlineHeaderHash)
-              if (headerDOMNode) {
-                headerDOMNode.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' })
-                setTimeout(() => (window.location.hash = outlineHeaderHash), 700)
-              }
-            }}
-            key={i}
-          >
-            {header.text}
-          </OutlineHeader>
-        ))}
-    </div>
-  )
-}
-
-export const OutlineLabel = styled.h3`
-  margin-bottom: 0px;
-  align-self: flex-start;
-  font-weight: 200;
-  font-family: 'Roboto-Light', 'Roboto Light', 'Roboto';
-  font-style: normal;
-  font-size: 20px;
-  color: ${props => props.theme.secondaryColor};
-  line-height: 24px;
-`
 
 export const RandomLineThatGoesAcrossTheContent = styled.div`
   width: 100%;
@@ -151,7 +105,7 @@ export const RandomLineThatGoesAcrossTheContent = styled.div`
   left: 0;
   position: absolute;
   border-bottom: 1px solid #c8ccd0;
-`
+`;
 
 export default class extends React.Component<
   {
@@ -171,38 +125,22 @@ export default class extends React.Component<
 > {
   state = {
     focused: false,
-  }
+  };
 
   render () {
     const {
       getFieldDecorator,
       setFieldsValue,
-      getFieldValue,
       getFieldError,
       text,
-      username,
-      userId,
-      ownerId,
-      userAvatar,
-    } = this.props
-
-    const editorState = typeof getFieldValue('text') === 'string' && getFieldValue('text')
-
-    const outlineHeadings =
-      typeof editorState === 'object' &&
-      (editorState.markdown
-        ? contentStateFromHTML(getHTMLFromMarkdown(editorState.markdown))
-          .getBlocksAsArray()
-          .map(block => block.toJS())
-          .filter(block => block.type.includes('header-one'))
-          .map(header => header.text)
-        : editorState.blocks &&
-          editorState.blocks.filter(block => block.type.includes('header-one')).map(header => header.text))
+    } = this.props;
 
     return (
       <SubmitArticleFormContent>
         <RandomLineThatGoesAcrossTheContent />
-        <SubmitArticleFormContainer onClick={() => this.setState({ focused: true })}>
+        <SubmitArticleFormContainer
+          onClick={() => this.setState({ focused: true })}
+        >
           <SubmitArticleFormText
             getFieldError={getFieldError}
             text={text}
@@ -210,17 +148,7 @@ export default class extends React.Component<
             getFieldDecorator={getFieldDecorator}
           />
         </SubmitArticleFormContainer>
-        <SubmitArticleFormDetails isSubmitting type='outline'>
-          <Outline
-            pageType='SubmittingArticle'
-            headings={outlineHeadings || []}
-            username={username}
-            userId={userId && userIdTrim(userId)}
-            userAvatar={userAvatar}
-            text={ownerId ? 'OWNER' : 'AUTHOR'}
-          />
-        </SubmitArticleFormDetails>
       </SubmitArticleFormContent>
-    )
+    );
   }
 }
