ff3bf64b4659dabc16eb3b1a9e4598bd201f74cf
Eric Juta <ericjohn.juta@consensys.net>
2018-11-23 15:29:02 +0000

    Fix kauri-components build

--- packages/kauri-components/babel.config.js
+++ packages/kauri-components/babel.config.js
@@ -1,1 +1,6 @@
-module.exports = require('../kauri-web/babel.config.js');
+const babelConfig = require("../kauri-web/babel.config.js");
+const index = babelConfig.plugins.indexOf("react-hot-loader/babel");
+if (index > -1) {
+  babelConfig.plugins.splice(index, 1);
+}
+module.exports = babelConfig;
