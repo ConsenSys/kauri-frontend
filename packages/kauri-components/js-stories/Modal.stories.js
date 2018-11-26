b31308cdcd6a5113e825d69fb9d251af092d5c9f
Eric Juta <ericjohn.juta@consensys.net>
2018-11-26 17:11:48 +0000

    Because

--- packages/kauri-components/js-stories/Modal.stories.js
+++ packages/kauri-components/js-stories/Modal.stories.js
@@ -1,22 +1,26 @@
 // @flow
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import styled from 'styled-components'
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
-import Modal from '../components/Modal'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import styled from "styled-components";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import Modal from "../components/Modal";
 
 const mockStore = configureStore();
 
-const ModalChildren = () => <marquee>Because Nelson!</marquee>
+const ModalChildren = () => <marquee>Woo!</marquee>;
 
-storiesOf('Modal', module)
-  .addDecorator(getStory => <Provider store={mockStore({
-    modal: {
-      isModalOpen: true,
-      children: <ModalChildren />,
-    },
-  })}>{getStory()}</Provider>)
-  .add('opened', () => (
-    <Modal />
+storiesOf("Modal", module)
+  .addDecorator(getStory => (
+    <Provider
+      store={mockStore({
+        modal: {
+          isModalOpen: true,
+          children: <ModalChildren />,
+        },
+      })}
+    >
+      {getStory()}
+    </Provider>
   ))
+  .add("opened", () => <Modal />);
