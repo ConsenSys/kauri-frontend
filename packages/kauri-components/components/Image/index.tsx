9fa72d037d8d4b018b81588fe9b5e825a789281c
Davide Scalzo <scalzodavide@yahoo.it>
2019-01-29 16:11:17 +0100

    lazyload curated list image

--- packages/kauri-components/components/Image/index.tsx
+++ packages/kauri-components/components/Image/index.tsx
@@ -9,32 +9,64 @@ interface ImgProps {
   inView?: boolean;
   borderTopLeftRadius?: string;
   borderTopRightRadius?: string;
+  borderBottomLeftRadius?: string;
+  borderBottomRightRadius?: string;
   children?: any;
+  overlay?: {
+    color?: string;
+    opacity: number;
+  };
+  asBackground?: boolean;
 }
 
 const Img = styled.div<ImgProps>`
-    height: ${props => props.height};
-    width: ${props => props.width};
-    border-radius: ${props => props.borderRadius};
-    background: ${props =>
-      `${props.theme.colors.white} ${
-        props.inView ? `url(${props.image})` : ``
-      }`};
-    background-size: cover;
-    opacity: ${props => (props.inView ? 1 : 0)};
-    transition: opacity 1s;
-    border-top-left-radius: ${props => props.borderTopLeftRadius};
-    border-top-right-radius ${props => props.borderTopRightRadius};
+  height: ${props => props.height};
+  width: ${props => props.width};
+  ${props => props.borderRadius && `border-radius: ${props.borderRadius}`};
+  background: ${props =>
+    `${props.inView ? `url(${props.image}) center center` : ``}`};
+  background-size: cover;
+  opacity: ${props => (props.inView ? 1 : 0)};
+  transition: opacity 1s;
+  ${props =>
+    props.borderTopLeftRadius &&
+    `border-top-left-radius: ${props.borderTopLeftRadius}`};
+  ${props =>
+    props.borderTopRightRadius &&
+    `border-top-right-radius: ${props.borderTopRightRadius}`};
+  position: ${props => (props.asBackground ? "absolute" : "relative")};
+
+  ${props =>
+    props.overlay &&
+    `&:after {
+      content: "";
+      opacity: ${props.overlay.opacity};
+      background: ${props.overlay.color || props.theme.colors.bgPrimary};
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      ${props.borderRadius && `border-radius: ${props.borderRadius}`};
+      ${props.borderTopLeftRadius &&
+        `border-top-left-radius: ${props.borderTopLeftRadius}`};
+      ${props.borderTopRightRadius &&
+        `border-top-right-radius: ${props.borderTopRightRadius}`};
+      ${props.borderBottomLeftRadius &&
+        `border-bottom-left-radius: ${props.borderBottomLeftRadius}`};
+      ${props.borderBottomRightRadius &&
+        `border-bottom-right-radius: ${props.borderBottomRightRadius}`};
+    }`}
 `;
 
 const Image = (props: ImgProps) => (
   <InView>
     {({ inView, ref }) => (
-      <div ref={ref}>
-        <Img {...props} inView={inView}>
-          {props.children}
-        </Img>
-      </div>
+      <Img innerRef={ref} {...props} inView={inView}>
+        {props.children}
+      </Img>
     )}
   </InView>
 );
--- packages/kauri-components/js-stories/Image.stories.tsx
+++ packages/kauri-components/js-stories/Image.stories.tsx
@@ -2,14 +2,25 @@ import { storiesOf } from "@storybook/react";
 import Image from "../components/Image";
 import React from "react";
 
-storiesOf("Image", module).add("FadeIn", () => (
-  <>
-    <div style={{ height: 1000 }}>Scroll down</div>
+storiesOf("Image", module)
+  .add("FadeIn", () => (
+    <>
+      <div style={{ height: 1000 }}>Scroll down</div>
+      <Image
+        image="https://images.unsplash.com/photo-1548600916-dc8492f8e845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        height="100px"
+        width="100px"
+        borderRadius="40px"
+      />
+    </>
+  ))
+  .add("Overlay", () => (
     <Image
-      image="https://images.unsplash.com/photo-1548600916-dc8492f8e845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+      image="https://images.unsplash.com/photo-1548606056-2093f8c13714?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
       height="100px"
       width="100px"
-      borderRadius="40px"
+      borderRadius="50px"
+      borderTopLeftRadius="2px"
+      overlay={{ opacity: 0.5, color: "red" }}
     />
-  </>
-));
+  ));
