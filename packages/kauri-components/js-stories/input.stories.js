05c931c36de1433055f274d646a2d746e045c7bf
Davide Scalzo <scalzodavide@yahoo.it>
2019-03-06 15:42:56 +0100

    allow centered inputs if text align property is center

--- packages/kauri-components/js-stories/input.stories.js
+++ packages/kauri-components/js-stories/input.stories.js
@@ -1,38 +1,60 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import Input from '../components/Input/Input'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Input from "../components/Input/Input";
 
 const mockState = {
-    value: 'test'
+  value: "test",
 };
 
-const handleChange = (value) => {
-    console.log('HandleChange: ', value);
-    mockState.value = value;
-}
+const handleChange = value => {
+  console.log("HandleChange: ", value);
+  mockState.value = value;
+};
 
-storiesOf('Input', module)
-  .add('Input', () => (
-      <>
-        <Input
-            fontWeight="normal"
-            fontSize={6}
-            placeHolder="Add your name"
-        />
-        <br />
-        <Input
-            onChange={(e) => handleChange(e)}
-            value={mockState.value}
-            fontWeight="normal"
-            fontSize={6}
-            placeHolder="Add your username"
-        />
-        <br />
-        <Input
-            handleChange={(e) => handleChange(e)}
-            fontWeight="normal"
-            fontSize={6}
-            placeHolder="handleChange"
-        />
-    </>
-  ))
+storiesOf("Input", module).add("Input", () => (
+  <>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+      }}
+    >
+      <Input fontWeight="normal" fontSize={6} placeHolder="Add your name" />
+      <br />
+      <Input
+        onChange={e => handleChange(e)}
+        value={mockState.value}
+        fontWeight="normal"
+        fontSize={6}
+        placeHolder="Add your username"
+      />
+      <br />
+      <Input
+        handleChange={e => handleChange(e)}
+        fontWeight="normal"
+        fontSize={6}
+        placeHolder="handleChange"
+      />
+    </div>
+    <br />
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <p>Centered</p>
+      <Input
+        textAlign="center"
+        handleChange={e => handleChange(e)}
+        fontWeight="normal"
+        fontSize={6}
+        placeHolder="handleChange"
+      />
+    </div>
+  </>
+));
