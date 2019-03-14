eb5b5d35a6844ac1f07940f61b0757ad3cba0ef7
Davide Scalzo <davidescalzo@Davides-MacBook-Pro.local>
2019-03-14 17:20:07 +0000

    linked tabs to data

--- packages/kauri-web/components/containers/Community/View.tsx
+++ packages/kauri-web/components/containers/Community/View.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getCommunity_getCommunity } from '../../../queries/__generated__/getCommunity';
 import CommunityHeader from '../../../../kauri-components/components/Community/CommunityHeader';
 import Tabs from '../../../../kauri-components/components/Tabs';
+import DisplayResources from './DisplayResources';
 
 interface IProps {
   data: {
@@ -16,6 +17,9 @@ class CommunityConnection extends React.Component<IProps> {
     }
 
     const {data: {getCommunity}} = this.props;
+    // TODO Change pending to approved once dev data is approved
+    const articles = getCommunity.pending && getCommunity.pending.filter( i => i && i.__typename === 'ArticleDTO');
+    const collections = getCommunity.pending && getCommunity.pending.filter( i => i && i.__typename === 'CollectionDTO');
       return <>
           <CommunityHeader
           avatar={String(getCommunity.avatar)}
@@ -29,11 +33,14 @@ class CommunityConnection extends React.Component<IProps> {
           tags={getCommunity.tags}
           members={getCommunity.members}
         />
-        {console.log(getCommunity)}
         <Tabs
           dark={true}
-          tabs={[{name: 'Home'}, {name: 'Articles'}, {name: 'Collections'}]}
-          panels={[<div>Home</div>, <div>Articles</div>, <div>Collections</div>]}
+          tabs={[{name: `Home`}, {name: `Articles (${articles && articles.length})`}, {name: `Collections (${collections && collections.length})`}]}
+          panels={[
+          <DisplayResources key="home" resources={getCommunity.pending} />,
+          <DisplayResources key="articles" resources={articles} />,
+          <DisplayResources key="collections" resources={collections} />,
+        ]}
       />
       </>
   }
