cdd8c85a1233df4f1d4b15f4e7ebcb214e81733f
Davide Scalzo <scalzodavide@yahoo.it>
2019-03-27 11:19:22 +0000

    fixed input (#487)

--- packages/kauri-web/components/containers/LoginForm/View.js
+++ packages/kauri-web/components/containers/LoginForm/View.js
@@ -70,9 +70,7 @@ const Web3Unavailable = () => (
       <style
         type="text/css"
         dangerouslySetInnerHTML={{
-          __html: `	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */`,
+          __html: `	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }`,
         }}
       />
       <div id="mc_embed_signup">
@@ -95,7 +93,6 @@ const Web3Unavailable = () => (
               </label>
               <input
                 type="email"
-                value=""
                 name="EMAIL"
                 className="required email"
                 id="mce-EMAIL"
