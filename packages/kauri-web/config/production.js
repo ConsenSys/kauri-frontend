dccce799256101ef66b653729cb75385ac1abbf7
Davide Scalzo <scalzodavide@yahoo.it>
2019-04-08 16:06:05 +0100

    Mixpanel tracking (#503)
    * add to collection
    
    * checkpoint and create/update collection
    
    * drafts and upvote tracking
    
    * drafts and upvote tracking
    
    * upvote and share
    
    * search tracking
    
    * outline click
    
    * remove legacy analytics
    
    * make email required
    
    * newsletter confirmation page
    
    * conditional website in community
    
    * better checking of dev environment
    
    * move mixpanel keys to config file

--- packages/kauri-web/config/production.js
+++ packages/kauri-web/config/production.js
@@ -7,7 +7,7 @@ module.exports = {
   gethBlockchain: process.env.GETH_BLOCKCHAIN,
   KauriCommunityId: process.env.KAURI_COMMUNITY_ID,
   analyticsTokens: {
-    mixpanel: process.env.MIXPANEL_TOKEN,
+    mixpanel: "7d83001be784f09b212b9b3274e41530",
     ga: "UA-112179323-1",
   },
 };
