4ef741327a0c88963ddedab6113a50a9333b0320
Davide Scalzo <davidescalzo@Davides-MacBook-Pro.local>
2019-04-29 15:32:44 +0100

    show communities on quick search

--- packages/kauri-web/components/containers/QuickSearch/View.tsx
+++ packages/kauri-web/components/containers/QuickSearch/View.tsx
@@ -117,7 +117,6 @@ class NavSearch extends React.Component<IProps, IState> {
       }))
       .subscribe(
         (dataSource: IDataSource) => {
-          delete dataSource.totalElementsBreakdown.COMMUNITY;
           if (dataSource.results.length === 0) {
             dataSource = EmptyData;
           }
