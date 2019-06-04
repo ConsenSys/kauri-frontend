7f42ab030260a02085e67afaa4d201ee08d25346
Davide Scalzo <scalzodavide@yahoo.it>
2019-06-04 15:59:21 -0500

    render content

--- packages/kauri-web/components/containers/ArticleDraft/DraftArticleContent.tsx
+++ packages/kauri-web/components/containers/ArticleDraft/DraftArticleContent.tsx
@@ -1,1 +1,15 @@
-export default () => <div>Content</div>;
+import Showdown from "showdown";
+
+const converter = new Showdown.Converter();
+
+interface IProps {
+  content: string;
+}
+
+export default ({ content }: IProps) => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: converter.makeHtml(JSON.parse(content).markdown),
+    }}
+  />
+);
