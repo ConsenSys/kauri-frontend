6a597aee82cb7a5ec1eff3bb1f4c12f669cff7f0
Eric Juta <ericjohn.juta@consensys.net>
2019-06-19 12:19:54 +0100

    Update empty state for community homepage designs

--- packages/kauri-web/components/containers/Community/EmptyStates/Homepage.tsx
+++ packages/kauri-web/components/containers/Community/EmptyStates/Homepage.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import styled from "../../../../lib/styled-components";
 import { routeChangeAction } from "../../../../lib/Module";
+import { BodyCard } from "../../../../../kauri-components/components/Typography";
+import Input from "../../../../../kauri-components/components/Input/Input";
+import AddOptions from "../../../../../kauri-components/components/AddOptions";
 
 const Container = styled.div`
   display: flex;
@@ -12,6 +15,21 @@ const Container = styled.div`
   & > svg {
     margin-bottom: ${props => props.theme.space[2]}px;
   }
+  cursor: pointer;
+`;
+
+const OpacityOverlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  opacity: 0.3;
+  > :first-child {
+    margin-bottom: ${props => props.theme.space[0]}px;
+  }
+  > :last-child {
+    margin-top: ${props => props.theme.space[2]}px;
+  }
+  margin-bottom: ${props => props.theme.space[3]}px;
 `;
 
 interface IProps {
@@ -21,13 +39,30 @@ interface IProps {
 
 const CommunityHomepageEmptyState: React.FunctionComponent<IProps> = props => (
   <Container>
-    <p
+    <OpacityOverlay
       onClick={() =>
         props.routeChangeAction(`/community/${props.id}/update-community`)
       }
     >
-      Show admin visible empty state here
-    </p>
+      <Input
+        placeHolder="Add Section Name"
+        fontSize={5}
+        fontWeight={500}
+        color={"primaryTextColor"}
+        textAlign={"center"}
+      />
+      <Input
+        placeHolder="Add Section Description"
+        fontSize={3}
+        fontWeight={500}
+        color={"primaryTextColor"}
+        textAlign={"center"}
+      />
+      <AddOptions>
+        <div />
+      </AddOptions>
+    </OpacityOverlay>
+    <BodyCard>Kendall to git commit here empty state copy :)</BodyCard>
   </Container>
 );
 
