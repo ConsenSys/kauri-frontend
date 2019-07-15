108f48fd0130c9cf0334607856eb283e7bfaf521
Davide Scalzo <scalzodavide@yahoo.it>
2019-07-15 15:17:59 +0100

    fix draft buttons when there is a background image on draft page (#619)

--- packages/kauri-web/__tests__/lib/mock-redux-initial-state.ts
+++ packages/kauri-web/__tests__/lib/mock-redux-initial-state.ts
@@ -4,9 +4,9 @@ const mockInitialState: IReduxState = {
   app: {
     hostName: "api.dev.kauri.io",
     user: {
-      id: "1234567890",
       avatar:
-        "https://pbs.twimg.com/profile_images/1116882135519059969/ju1J_WpA_reasonably_small.jpg",
+      "https://pbs.twimg.com/profile_images/1116882135519059969/ju1J_WpA_reasonably_small.jpg",
+      id: "1234567890",
       username: "rej156",
       communities: [],
       status: "EMAIL_VERIFIED",
